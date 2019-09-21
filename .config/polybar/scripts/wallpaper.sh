0c056b965c8295b236d516cfe20bf7d274f4f0e2
euiciiowr <euiciiowr@outlook.com>
2019-09-21 12:51:59 +0000

    add folder /scripts and format basics settings

--- .config/polybar/scripts/wallpaper.sh
+++ .config/polybar/scripts/wallpaper.sh
@@ -6,7 +6,7 @@
 dir="${HOME}/Imagens/Wallpapers/"
 unsplash_dir="${XDG_PICTURES_DIR:-${HOME}/img}/unsplash"
 default="$dir/Imagens/Wallpapers/wallhaven-726661_1920x1080.png"
-ultima="/home/ghost/Imagens/Wallpapers/wallhaven-83dy9k_1920x1080.png"
+ultima="/home/ghost/Imagens/Wallpapers/wallhaven-724791_1920x1080.png"
 modo="--bg-fill"
 indice=0
 i=0
